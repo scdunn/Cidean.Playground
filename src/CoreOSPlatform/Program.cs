bd253e9cf6c07cf3608460ab85b0a8711c6ffde1
scdunn <chris@cidean.com>
2019-03-05 10:16:25 -0500

    add project core os

---
+++ src/CoreOSPlatform/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace CoreOSPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CurrentOS.ISOSLinux())
+                Console.WriteLine("OS is Linux");
+            else if (CurrentOS.IsOSWindows())
+                Console.WriteLine("OS is Windows");
+            else if (CurrentOS.ISOSLinux())
+                Console.WriteLine("OS is OSX");
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class CurrentOS
+    {
+        public static bool IsOSWindows () => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static bool ISOSLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        public static bool IsOSOSX() => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+    }
+}
